8aa61cdb83a83307a92bb5a8d6dfd976b5a794d9
helenblanc <helen.blanc.campos@gmail.com>
2023-02-01 19:31:12 -0300

    corrección boton

--- src/Components/CustomForm.jsx
+++ src/Components/CustomForm.jsx
@@ -1,14 +1,11 @@
-import { Component } from 'react';
 import { useState } from "react";
 
-import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Form from 'react-bootstrap/Form';
 import Alert from 'react-bootstrap/Alert';
-
+import Button from 'react-bootstrap/Button';
 
 const CustomForm = () => {
-  console.log('create custom form')
   // Creación variables formulario
   // Creación variable nombre de usuario
   const [username, setUsername] = useState("")
@@ -21,28 +18,29 @@ const CustomForm = () => {
 
   // Función para validar el formulario
   const login = (event) => {
+    console.log("login");
+    console.log("username: ", username);
+    console.log("password: ", password);
+
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
       event.preventDefault();
       event.stopPropagation();
     }
+    if (username === "ADL" && password === "252525") {
 
-    if (username === "" || password === "") {
-      setError(true)
+      setError(false)
+      alert('Login correcto');
       return
-    }else{
-      if(username === "ADL" && password === "252525"){
-        setError(false)
-        alert('Login correcto');
-      }
-      else{
-        setError(true);
-      }
     }
-
-    setError(false);
+    else {
+      setError(true);
+      
+      return
+    }
   };
 
+  // Función para habilitar el botón
   const habilitar = () => {
     if (username !== "" && password !== "") {
       setDisabledButton(false)
@@ -52,19 +50,19 @@ const CustomForm = () => {
   }
 
   return (
-    <div  style={{ maxWidth: "700px", margin: '0 auto', alignItems: "center", justifyContent: "center", marginTop: '20px', marginBottom: '20px' }}>
-      {error ? <Alert key='danger' variant='danger' >Los datos son incorrectos. !</Alert>: null}
-      <Form noValidate validated={!error} onSubmit={login} >
-        <Form.Group as={Row} className="mb-3" controlId="txt.usernameInput">
+    <div style={{ maxWidth: "700px", margin: '0 auto', alignItems: "center", justifyContent: "center", marginTop: '20px', marginBottom: '20px' }}>
+      {error ? <Alert key='danger' variant='danger' >Los datos son incorrectos. !</Alert> : null}
+      <Form noValidate validated={!error} onSubmit={(e) => login(e)}>
+        <Form.Group className="mb-3" controlId="txt.usernameInput">
           <Form.Label className='ml-auto'>USERNAME</Form.Label>
-          <Form.Control type="text" placeholder="" name='username' onChange={(e) => {setUsername(e.target.value);habilitar()}} />
+          <Form.Control type="text" placeholder="" name='username' onChange={(e) => { setUsername(e.target.value); habilitar() }} />
         </Form.Group>
-        <Form.Group as={Row} className="mb-3" controlId="txt.passInput">
+        <Form.Group className="mb-3" controlId="txt.passInput">
           <Form.Label>PASSWORD</Form.Label>
-          <Form.Control type="password" placeholder="" name='password' onChange={(e) => {setPassword(e.target.value);habilitar()}} />
+          <Form.Control type="password" placeholder="" name='password' onChange={(e) => { setPassword(e.target.value); habilitar() }} />
         </Form.Group>
+        {!disabledButton ? <Button type="submit" variant="dark">Iniciar Sesión</Button> : null}
       </Form>
-      {!disabledButton ? <Button type="submit" variant="dark">Iniciar Sesión</Button> : null}
     </div>
   );
 };
